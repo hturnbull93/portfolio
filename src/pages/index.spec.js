d6efdc021a157c00bf9df1d2ecd0f85ccb1a7b70
Harry Turnbull <hturnbull@live.co.uk>
2020-06-15 16:09:23 +0100

    Add Home snapshot test

--- src/pages/index.spec.js
+++ src/pages/index.spec.js
@@ -1,8 +1,14 @@
 import React from "react"
 import { shallow } from "enzyme"
+import renderer from "react-test-renderer"
 import Home from "."
 
 describe("Home", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Home />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
   it("has a h1 containing Harry Turnbull", () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find("h1").text()).toEqual("Harry Turnbull")
