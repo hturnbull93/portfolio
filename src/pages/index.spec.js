1f9674991df9e93060e5450c0b23fab7cc1b9c7d
Harry Turnbull <hturnbull@live.co.uk>
2020-06-16 22:03:22 +0100

    Specify index in Home import

--- src/pages/index.spec.js
+++ src/pages/index.spec.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { shallow } from "enzyme"
 import renderer from "react-test-renderer"
-import Home from "."
+import Home from "./index"
 
 describe("Home", () => {
   it("matches snapshot", () => {
