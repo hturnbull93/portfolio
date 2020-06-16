2c519b4bf4ac24286a4ecf14536f0d3fe1857c68
Harry Turnbull <hturnbull@live.co.uk>
2020-06-16 23:33:42 +0100

    R: Home renders a Layout

--- src/pages/index.spec.js
+++ src/pages/index.spec.js
@@ -9,6 +9,11 @@ describe("Home", () => {
     expect(tree).toMatchSnapshot()
   })
 
+  it("renders a Layout", () => {
+    const wrapper = shallow(<Home />)
+    expect(wrapper.find("Layout").length).toEqual(1)
+  })
+
   it("has a h1 containing Harry Turnbull", () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find("h1").text()).toEqual("Harry Turnbull")
