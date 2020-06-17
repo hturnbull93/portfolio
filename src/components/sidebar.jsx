db202658905df914baf47f5c03d8eeeb642b640f
Harry Turnbull <hturnbull@live.co.uk>
2020-06-17 13:38:40 +0100

    Add Sidebar SASS and structure

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,13 +1,34 @@
 import React from "react"
+import style from "./sidebar.scss"
 
 const Sidebar = () => {
   return (
-    <div>
-      <a href="https://github.com/hturnbull93/cv">CV</a>
-      <a href="https://twitter.com/hturnbull">Twitter</a>
-      <a href="https://www.linkedin.com/in/harry-turnbull">LinkedIn</a>
-      <a href="https://github.com/hturnbull93">GitHub</a>
-      <a href="https://medium.com/@hturnbull93">Medium Blog</a>
+    <div className="sidebar border">
+      <h3>
+        Harry Turnbull <small>Full Stack Developer</small>
+      </h3>
+      <nav>
+        <section>
+          <h4>Links</h4>
+          <ul>
+            <li>
+              <a href="https://github.com/hturnbull93/cv">GitHub CV</a>
+            </li>
+            <li>
+              <a href="https://twitter.com/hturnbull">Twitter</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/harry-turnbull">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://github.com/hturnbull93">GitHub</a>
+            </li>
+            <li>
+              <a href="https://medium.com/@hturnbull93">Medium Blog</a>
+            </li>
+          </ul>
+        </section>
+      </nav>
     </div>
   )
 }
