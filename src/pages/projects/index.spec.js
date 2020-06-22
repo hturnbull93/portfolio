8b235b24c79b32c83cabcaeb7681e9386b62371f
Harry Turnbull <hturnbull@live.co.uk>
2020-06-22 15:47:39 +0100

    Specify Projects import file

--- src/pages/projects/index.spec.js
+++ src/pages/projects/index.spec.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { shallow } from "enzyme"
 import renderer from "react-test-renderer"
-import Projects from "."
+import Projects from ".index"
 
 describe("Projects", () => {
   it("renders a Layout", () => {
