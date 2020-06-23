11a0b9d34e7d44c0746e26760ddf42f4b8739382
Harry Turnbull <hturnbull@live.co.uk>
2020-06-23 18:22:56 +0100

    R: Projects renders a ProjectCard for each project

--- src/pages/projects/index.spec.js
+++ src/pages/projects/index.spec.js
@@ -2,15 +2,60 @@ import React from "react"
 import { shallow } from "enzyme"
 import renderer from "react-test-renderer"
 import Projects from "."
+import ProjectCard from "../../components/projectCard"
 
 describe("Projects", () => {
   it("renders a Layout", () => {
-    const wrapper = shallow(<Projects />)
+    const wrapper = shallowWithData()
     expect(wrapper.find("Layout").length).toEqual(1)
   })
 
   it("has a h1 containing Projects", () => {
-    const wrapper = shallow(<Projects />)
+    const wrapper = shallowWithData()
     expect(wrapper.find("h1").text()).toEqual("Projects")
   })
+
+  it("renders a ProjectCard for each project", () => {
+    const wrapper = shallowWithData()
+    const id = "1"
+    const details = {
+      title: "Test Title",
+      repoLink: "https://github.com/hturnbull93/example",
+      deployLink: "https://example-deploy-link.com",
+      tech: "Test tech",
+      img: "/test-image.png",
+      description: "Test description",
+      link: "/projects/example-project",
+      label: "Personal",
+    }
+    const projectCard = <ProjectCard key={id} details={details} />
+
+    expect(wrapper.containsMatchingElement(projectCard)).toEqual(true)
+  })
 })
+
+const shallowWithData = () => {
+  const data = {
+    allMarkdownRemark: {
+      edges: [
+        {
+          node: {
+            id: "1",
+            frontmatter: {
+              title: "Test Title",
+              repoLink: "https://github.com/hturnbull93/example",
+              deployLink: "https://example-deploy-link.com",
+              tech: "Test tech",
+              img: "/test-image.png",
+              description: "Test description",
+              link: "/projects/example-project",
+              label: "Personal",
+            },
+          },
+        },
+      ],
+    },
+  }
+
+  return shallow(<Projects data={data} />)
+}
