1476b843a2921e0ca97953db468b17eb24457c0d
Harry Turnbull <hturnbull@live.co.uk>
2020-06-24 14:54:05 +0100

    RM: unused border class on div

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,15 +4,13 @@ import { Link } from "gatsby"
 
 const Navbar = () => {
   return (
-    <div className={`${style.sidebar} border`}>
-      <nav>
+      <nav className={style.navbar}>
         <ul>
           <li>
             <Link to="/">About</Link>
           </li>
         </ul>
       </nav>
-    </div>
   )
 }
 
