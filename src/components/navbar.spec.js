2ad6c38a6a58765b69bab60f5ffc9bcf0c152929
Harry Turnbull <hturnbull@live.co.uk>
2020-06-26 16:22:35 +0100

    R: Navbar renders Link to projects

--- src/components/navbar.spec.js
+++ src/components/navbar.spec.js
@@ -5,11 +5,15 @@ import Navbar from "./navbar"
 import { Link } from "gatsby"
 
 describe("Navbar", () => {
-  describe("internal navigation", () => {
-    it("renders Link to home/about", () => {
-      const wrapper = shallow(<Navbar />)
-      const link = <Link to="/">About</Link>
-      expect(wrapper.containsMatchingElement(link)).toEqual(true)
-    })
+  it("renders Link to home/about", () => {
+    const wrapper = shallow(<Navbar />)
+    const link = <Link to="/">About</Link>
+    expect(wrapper.containsMatchingElement(link)).toEqual(true)
+  })
+
+  it("renders Link to projects", () => {
+    const wrapper = shallow(<Navbar />)
+    const link = <Link to="/projects">Projects</Link>
+    expect(wrapper.containsMatchingElement(link)).toEqual(true)
   })
 })
