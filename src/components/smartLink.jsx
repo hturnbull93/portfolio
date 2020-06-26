a62d960cf40c12e3eb9dff19803b7b07b9fc15db
Harry Turnbull <hturnbull@live.co.uk>
2020-06-26 14:09:35 +0100

    G: SmartLink renders an a tag when passed a href prop

--- src/components/smartLink.jsx
+++ src/components/smartLink.jsx
@@ -1,8 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const SmartLink = ({ to, children }) => {
-  return <Link to={to}>{children}</Link>
+const SmartLink = ({ to, href, children }) => {
+  if (to) return <Link to={to}>{children}</Link>
+  if (href) return <a href={href}>{children}</a>
 }
 
 export default SmartLink
