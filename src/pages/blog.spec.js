c87e5171f7dd82624d63338f69d191ad32a071ad
Harry Turnbull <hturnbull@live.co.uk>
2020-07-07 13:43:41 +0100

    R: Blog renders a layout

---
+++ src/pages/blog.spec.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { shallow } from "enzyme"
+import renderer from "react-test-renderer"
+import Blog from "./blog"
+
+describe("Blog", () => {
+  it("renders a Layout", () => {
+    const wrapper = shallowWithData()
+    expect(wrapper.find("Layout").length).toEqual(1)
+  })
+})
+
+const shallowWithData = () => {
+  const data = {
+    allMediumFeed: {
+      edges: [
+        {
+          node: {
+            id: "abc",
+            link: "https://example.com/abc",
+            title: "Blog abc",
+            date: "7th July, 2020",
+          },
+        },
+        {
+          node: {
+            id: "123",
+            link: "https://example.com/abc",
+            title: "Blog 123",
+            date: "18th June, 2020",
+          },
+        },
+      ],
+    },
+  }
+
+  return shallow(<Blog data={data} />)
+}
