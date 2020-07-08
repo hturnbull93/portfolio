c04202b4f225e6e06eb26f77455afc1c21215549
Harry Turnbull <hturnbull@live.co.uk>
2020-07-08 09:55:03 +0100

    Add graphql query for Blog

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,8 +1,24 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
 const Blog = () => {
   return <Layout></Layout>
 }
 
 export default Blog
+
+export const query = graphql`
+  {
+    allMediumFeed {
+      edges {
+        node {
+          id
+          link
+          title
+          date(formatString: "Do MMMM, YYYY")
+        }
+      }
+    }
+  }
+`
