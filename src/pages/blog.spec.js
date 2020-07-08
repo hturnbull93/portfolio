ef9469cf3727e552d4ffbe04ac53538db72eeca7
Harry Turnbull <hturnbull@live.co.uk>
2020-07-08 10:43:24 +0100

    R: Blog renders a BlogPost for each post

--- src/pages/blog.spec.js
+++ src/pages/blog.spec.js
@@ -2,11 +2,38 @@ import React from "react"
 import { shallow } from "enzyme"
 import renderer from "react-test-renderer"
 import Blog from "./blog"
+import BlogPost from "../components/blogPost"
 
 describe("Blog", () => {
   it("renders a Layout", () => {
     const wrapper = shallowWithData()
     expect(wrapper.find("Layout").length).toEqual(1)
+  })
+
+  it("renders a BlogPost for each post", () => {
+    const wrapper = shallowWithData()
+    const postOne = (
+      <BlogPost
+        details={{
+          id: "abc",
+          link: "https://example.com/abc",
+          title: "Blog abc",
+          date: "7th July, 2020",
+        }}
+      />
+    )
+    const postTwo = (
+      <BlogPost
+        details={{
+          id: "123",
+          link: "https://example.com/abc",
+          title: "Blog 123",
+          date: "18th June, 2020",
+        }}
+      />
+    )
+    expect(wrapper.containsMatchingElement(postOne)).toEqual(true)
+    expect(wrapper.containsMatchingElement(postTwo)).toEqual(true)
   })
 })
 
