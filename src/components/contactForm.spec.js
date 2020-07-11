878e003ad3474fadebbf3bd0d6af4bcee0cf6ac2
Harry Turnbull <hturnbull@live.co.uk>
2020-07-11 16:48:50 +0100

    RF: use regular fetch with jest-fetch-mock

--- src/components/contactForm.spec.js
+++ src/components/contactForm.spec.js
@@ -2,12 +2,13 @@ import React from "react"
 import { shallow, mount } from "enzyme"
 import axios from "axios"
 import ContactForm from "./contactForm"
+import fetchMock from "jest-fetch-mock"
 
 describe("ContactForm", () => {
   it("form submit causes axios post", done => {
     const mockSuccessResponse = { status: 200 }
     const mockFetchPromise = Promise.resolve(mockSuccessResponse)
-    jest.spyOn(axios, "post").mockImplementation(() => mockFetchPromise)
+    jest.spyOn(global, "fetch").mockImplementation(() => mockFetchPromise)
 
     const wrapper = mount(<ContactForm />)
     wrapper.setState({
@@ -18,14 +19,15 @@ describe("ContactForm", () => {
     const form = wrapper.find("form")
     form.simulate("submit")
 
-    expect(axios.post).toHaveBeenCalledTimes(1)
-    expect(axios.post).toHaveBeenCalledWith("/", {
-      headers: { "content-type": "application/x-www-form-urlencoded" },
+    expect(global.fetch).toHaveBeenCalledTimes(1)
+    expect(global.fetch).toHaveBeenCalledWith("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body:
         "form-name=contact&name=test&email=test%40example.com&message=hello%20world",
     })
 
-    axios.post.mockClear()
+    global.fetch.mockClear()
     done()
   })
 })
