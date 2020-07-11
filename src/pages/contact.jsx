d331004fa6df465d0ddb1a522a202e578f71159f
Harry Turnbull <hturnbull@live.co.uk>
2020-07-11 22:24:42 +0100

    Import SEO

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import ContactForm from "../components/contactForm"
 import Watermark from "../components/watermark"
+import SEO from "../components/seo"
 
 const Contact = () => {
   return (
