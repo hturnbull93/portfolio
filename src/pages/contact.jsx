d1d8191b44dc6b182733ee102cb27f04506bb6db
Harry Turnbull <hturnbull@live.co.uk>
2020-07-11 21:55:55 +0100

    RF: Add watermark title

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,20 +1,23 @@
 import React from "react"
 import Layout from "../components/layout"
 import ContactForm from "../components/contactForm"
+import Watermark from "../components/watermark"
 
 const Contact = () => {
   return (
     <Layout>
+      <Watermark>
+        <h1>Contact.</h1>
+      </Watermark>
       <p>
-        email at {" "}
-        <a href="mailto:hturnbull@live.co.uk">hturnbull@live.co.uk</a>.
+        email at <a href="mailto:hturnbull@live.co.uk">hturnbull@live.co.uk</a>.
       </p>
       <p>
-        tweet at {" "}
-        <a href="https://twitter.com/hturnbull">@hturnbull93</a>.
+        tweet at <a href="https://twitter.com/hturnbull">@hturnbull93</a>.
       </p>
       <p>
-        connect on <a href="https://www.linkedin.com/in/harry-turnbull/">LinkedIn</a>.
+        connect on{" "}
+        <a href="https://www.linkedin.com/in/harry-turnbull/">LinkedIn</a>.
       </p>
       <p>
         follow on <a href="https://github.com/hturnbull93/">GitHub</a>.
